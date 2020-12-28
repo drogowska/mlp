d80e8d30b22c3bac5b0c9928d17b27817f0f80b8
drogowska <60966644+drogowska@users.noreply.github.com>
2020-12-28 20:35:33 +0100

    Update zad3.py

--- zad3.py
+++ zad3.py
@@ -1,23 +1,16 @@
 import math
 
 tab = []
+hidden = []
+layer = []
 
-def toInt(a, b, c,d):
-    a = int(a)
-    b = int(b)
-    c = int(c)
-    d = int(d)
-
-
-def readFromFile():
+def readFromFile():  #czyta jako stringa
     i = 0
     with open('transformation.txt') as file:
         for line in file.readlines():
-            a,b,c,d = line.strip().split(",")
-            toInt(a,b,c,d)
+            a, b, c, d = line.strip().split(",")
             tab.append([a, b, c, d])
             i+=1
-
 
 def neuron(x, w):
     suma = 0
@@ -25,8 +18,24 @@ def neuron(x, w):
         suma += x[i]*w[i]
     return suma
     
-def actFunction(x):
-    return 1/(1+math.e**(-1*x))
+def actFunction(y):              #wyjście y to wyniki funkcji nuron
+    return 1/(1+math.e**(-1*y))
 
+def changeW(w, x): #zmiana wag
+     for i in range(len(x)):
+        w[i] = w[i] - 2*alfa*(x[i]* d[i])
+     return w
+
+def f(z, y, howMuchLayers):
+    for p in range(howMuchLayers):
+        for j in range(len(y)):
+            d[p][j] = z[j] - y[j]
+
+
+def pochodna(z):
+    return actFunction(z) * (1 - actFunction(z))
+
+alfa = 0.1
+d =[]
 readFromFile()
 print(tab)
